d82552d6fb92018e4023da576383697a34dff12a
liuchang <liu.chang@chaott.net>
2017-10-13 15:25:20 +0800

    更新动态接口

--- application/controllers/Dynamic.php
+++ application/controllers/Dynamic.php
@@ -7,36 +7,57 @@ class Dynamic extends CI_Controller
 {
     public function index()
     {
-        $result = LoginService::check();
+        $latitude = $this->input->get('latitude')?:0;
+        $longitude = $this->input->get('longitude')?:0;
+        $range = $this->input->get('range')?:0.01;
 
-        // check failed
-        if ($result['code'] !== 0) {
-            return;
+        $this->db->select('open_id as id, master_name as masterName, master_avatar_url as masterAvatarUrl, dog_name as dogName, dog_avatar_url as dogAvatarUrl, marked_at as markedAt');
+        $this->db->from('dynamics');
+        if ($latitude && $longitude) {
+            $this->db->where([
+                'latitude <' => $latitude+$range,
+                'latitude >' => $latitude-$range,
+                'longitude <' => $longitude+$range,
+                'longitude >' => $longitude-$range
+            ]);
         }
+        $dynamicInfos = $this->db->order_by('marked_at', 'DESC')->limit(50)->get()->result_array();
+        //$dump($dynamicInfos);exit;
 
-        $open_id = $result['data']['userInfo']['openId'];
-        //根据openid 获取宠物信息
-        $dogInfo = $this->db->select('open_id as id, name, breed, avatar_url as avatarUrl')
-            ->where(['open_id'=> $open_id])
-            ->get('dogs')
-            ->row_array();
-        if ($dogInfo) {
-            $marker = $this->db->where('open_id', $open_id)->get('markers')->row();
-            if ($marker) {
-                $dogInfo['markedAt'] = $marker->marked_at;
-                $lastClockDay = substr($marker->marked_at, 0, 10);
-                if ($lastClockDay == date('Y-m-d')) {
-                    $dogInfo['clocked'] = true;
-                }
-            }
-        }
+        array_walk($dynamicInfos, function (&$item, $key) {
+            $item['showTime'] = $this->timeTranx($item['markedAt']);
+        });
+        //var_dump($dynamicInfos);exit;
 
         $response = array(
             'code' => 0,
             'message' => 'ok',
-            'data' => $dogInfo,
+            'data' => $dynamicInfos,
         );
 
         $this->output->set_content_type('application/json')->set_output(json_encode($response));
+    }
+
+    private function timeTranx($showTime)
+    {
+        $now = time();
+        $show = strtotime($showTime);
+        $dur = $now - $show;
+        if ($dur < 0) {
+            return $showTime;
+        }
+        if ($dur < 60) {
+            return $dur.'秒前';
+        }
+        if ($dur < 3600) {
+            return floor($dur/60).'分钟前';
+        }
+        if ($dur < 86400) {
+            return floor($dur/3600).'小时前';
+        }
+        if ($dur < 259200) {
+            return floor($dur/86400).'天前';
+        }
+        return $showTime;
     }
 }
